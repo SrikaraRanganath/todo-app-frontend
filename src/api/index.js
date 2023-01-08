70073edb66040717d42ac7cefad151ec4bde4424
srikarar <srikarar@gmail.com>
2023-01-08 10:59:18 +0530

    api link

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'http://localhost:5001' });
+const api = axios.create({ baseURL: 'https://todo-app-backend-tmfk.onrender.com' });
 api.interceptors.request.use((req) => {
     if(localStorage.getItem('profile')) {
         const profile = JSON.parse (localStorage.getItem('profile'));
